f707e1f3dbc08b9e4e19865129f7c2a8691d4d8a
nishu2811 <nishantranjan78@gmail.com>
2019-09-21 00:17:09 +0530

    renamed java file from exmple to RemoveSpecialCharacter

--- RemoveSpecialCharacter.java
+++ RemoveSpecialCharacter.java
@@ -2,7 +2,7 @@ package BasicInterviewCodes;
 
 import java.util.Scanner;
 
-public class Example {
+public class RemoveSpecialCharacter {
 	public static void main(String args[]) {
 		Scanner sc=new Scanner(System.in);
 		System.out.println("enter the input");
