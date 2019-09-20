5cc48b47b07b0bdb9226d3144f81d3553d78603b
nishu2811 <nishantranjan78@gmail.com>
2019-09-21 01:08:26 +0530

    Segerate Even Odd and print

--- roughWork.java
+++ roughWork.java
@@ -2,31 +2,14 @@ package BasicInterviewCodes;
 
 import java.util.Scanner;
 
-public class EvenOddJava {
+public class roughWork {
 	
-	static void arrangeEvenOdd(int arr[], int n) {
-		
-		int j=-1, temp;
-		
-		//Quick Sort method
-		for(int i=0; i<n; i++) {
-			//If array of element is odd then swap
-			if (arr[i]%2==0) {
-				j++;
-				
-				//swap the element
-				temp=arr[i];
-				arr[i]=arr[j];
-				arr[j]=temp;
-			}
-		}
-		
-	}
+	static int i,n;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int n;
-		Scanner s = new Scanner(System.in);
+		
+        Scanner s = new Scanner(System.in);
         System.out.print("Enter no. of elements you want in array:");
         n = s.nextInt();
         int a[] = new int[n];
@@ -36,12 +19,9 @@ public class EvenOddJava {
             a[i] = s.nextInt();
             //sum = sum + a[i];
         }
-        
-        arrangeEvenOdd(a, n);
 		
-        System.out.print("arranged array: ");
         for (int i = 0; i < n; i++) 
             System.out.print(a[i] + " ");
-	}
+    }
 
 }
